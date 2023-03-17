b09e98ad06c02ce8239e5a1307da78e69238bcac
AadityakiranS <aadityakirans@gmail.com>
2023-03-17 09:01:07 +0530

    Changed login route from being get to post

--- routes/router.js
+++ routes/router.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const { debug_deleteUser, signUpUser, loginUser, debug_getAllUsers, debug_getUserAuth } = require('../controller/api-requests.js');
 const { verifyAccessToken } = require('../helpers/jwt-helper.js');
 
-router.route('/').get((req, res, next) => { console.log("Hello"); next(); }, debug_getAllUsers);
-router.route('/login').get(loginUser);
+router.route('/').get(debug_getAllUsers);
+router.route('/login').post(loginUser);
 router.route('/signup').post(signUpUser);
 router.route('/:id').get(verifyAccessToken, debug_getUserAuth).delete(debug_deleteUser);
 
